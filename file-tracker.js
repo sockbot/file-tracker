aa48a9cbcffa80eaa6c2007d538860c3eef6ea22
Andrew Chan <andrew.chan@PeakInstallations.com>
2018-07-11 12:37:01 -0700

    Configure Datatables editor

--- file-tracker.js
+++ file-tracker.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
-  $('#main').DataTable(
+
+  var editor = new $.fn.dataTable.Editor( {} );
+
+  new $.fn.dataTable.Editor( {
+      // ajax:  '/api/staff',
+      table: '#myTable',
+      // fields: [
+      //     { label: 'First name', name: 'first_name' },
+      //     { label: 'Last name',  name: 'last_name'  },
+      //     // etc
+      // ]
+  } );
+
+  $('#file-tracker-table').DataTable(
     {
       paging: false,
       dom: 'Blfrtip',
+      select: true,
       buttons: [
-        'copy', 'excel', 'pdf'
+        'copy',
+        'excel',
+        'pdf',
+        { extend: 'create', editor: editor },
+        { extend: 'edit', editor: editor },
+        { extend: 'remove', editor: editor },
       ]
     }
   );
+
 })
